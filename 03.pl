72fd1360ff5cfcbec38a2d06775bd7c757e5ae82
Vlad Sokolov <sokol.metal@gmail.com>
2017-02-24 20:39:16 +0300

    first commit

---
+++ 03.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+=encoding UTF8
+=head1 SYNOPSYS
+
+Поиск наименьшего и наибольшего из 3-х чисел
+
+=head1 run ($x, $y, $z)
+
+Функция поиска минимального и максимального из 3-х чисел $x, $y, $z.
+Пачатает минимальное и максимальное числа, в виде "$value1, $value2\n"
+
+Примеры: 
+
+run(1, 2, 3) - печатает "1, 3\n".
+
+run(1, 1, 1) - печатает "1, 1\n"
+
+run(1, 2, 2) - печатает "1, 2\n"
+
+=cut
+
+sub run {
+    my ($x, $y, $z) = @_;
+    my $min = undef;
+    my $max = undef;
+
+    if ($x > $y) {
+        if ($z > $y) {
+            $min = $y;
+        } else {
+            $min = $z;
+        }
+        if ($x > $z) {
+            $max = $x;
+        } else {
+            $max = $z;
+        }
+    } else {
+        if ($x < $z) {
+            $min = $x;
+        } else {
+            $min = $z;
+        }
+        if ($y > $z) {
+            $max = $y;
+        } else {
+            $max = $z;
+        }
+            
+    }
+    
+    
+
+    print "$min, $max\n";
+}
+
+1;
